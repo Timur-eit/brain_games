f3480f45138d789643851644aed3db19f6061e1f
Evgeny Tsygankov <timur_eit@192.168.10.104>
2020-03-18 23:21:58 +0300

    add code for brain-even game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,61 @@
 import readlineSync from 'readline-sync';
 
-const nameRequest = () => {
+const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
 
-export default nameRequest;
+export default greeting;
+
+
+// brain-even
+
+export const ruleForBrainEven = 'Answer "yes" if the number is even, otherwise answer "no"';
+
+const getRandomNumber = () => {
+  const minNumber = 1;
+  const maxNumber = 100;
+  const random = () => Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+  return random(maxNumber);
+};
+
+const arrayOfRandom = [getRandomNumber(), getRandomNumber(), getRandomNumber()];
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
+
+export const brainEven = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(ruleForBrainEven);
+  // need to use for...of in this function
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of arrayOfRandom) {
+    const answer = readlineSync.question(`Question: ${item}\nYour answer: `);
+    const validAnswers = ['no', 'yes'];
+    const zero = validAnswers[0];
+    const one = validAnswers[1];
+    let trueAnswer;
+
+    if (isEven(item)) {
+      trueAnswer = one;
+    } else if (!isEven(item)) {
+      trueAnswer = zero;
+    }
+
+    if (answer === trueAnswer) {
+      console.log('Correct!');
+    } else if (answer !== trueAnswer) {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".\nLet's try again, ${userName}!`);
+      break;
+    }
+
+    console.log(`Congratulations, ${userName}`);
+  }
+};
