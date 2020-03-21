ba609fb9d9b4d3e55704b390a3ec3709d54f3c9c
Evgeny <evtsyga@gmail.com>
2020-03-21 17:11:26 +0300

    update index.js and add brain-calc

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-};
-
+const nameRequst = (question) => readlineSync.question(question);
+const greeting = () => console.log(`Hello, ${nameRequst('May I have your name? ')}!`);
 
 export default greeting;
 
-
-// brain-even
-
-export const ruleForBrainEven = 'Answer "yes" if the number is even, otherwise answer "no"';
-
-const getRandomNumber = () => {
-  const minNumber = 1;
-  const maxNumber = 100;
+const getRandomNumber = (min, max) => {
+  const minNumber = min;
+  const maxNumber = max;
   const random = () => Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
   return random(maxNumber);
 };
 
-const collOfRandomNumbers = [getRandomNumber(), getRandomNumber(), getRandomNumber()];
+/*
+/ BRAIN-EVEN
+*/
+
+const ruleForBrainEven = 'Answer "yes" if the number is even, otherwise answer "no"';
+const collOfRandomNumbers = [getRandomNumber(1, 100), getRandomNumber(1, 100), getRandomNumber(1, 100)];
 // 3 items of array => 3 questions to user
 
 const isEven = (number) => {
@@ -30,37 +27,100 @@ const isEven = (number) => {
   return false;
 };
 
-
 export const brainEven = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!\n${ruleForBrainEven}`);
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+  console.log(ruleForBrainEven);
 
   let result;
   // need to use "for...of" in this function instead of "Array.prototype.forEach()" due to "break"
   // eslint-disable-next-line no-restricted-syntax
   for (const item of collOfRandomNumbers) {
     const answer = readlineSync.question(`Question: ${item}\nYour answer: `);
-    const validAnswers = ['no', 'yes'];
-    const zero = validAnswers[0];
-    const one = validAnswers[1];
+
     let trueAnswer;
 
     if (isEven(item)) {
-      trueAnswer = one;
+      trueAnswer = 'yes';
     } else if (!isEven(item)) {
-      trueAnswer = zero;
+      trueAnswer = 'no';
     }
 
     if (answer === trueAnswer) {
       console.log('Correct!');
       result = true;
     } else if (answer !== trueAnswer) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".\nLet's try again, ${userName}!`);
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".\nLet's try again, ${username}!`);
       result = false;
       break;
     }
   }
   if (result === true) {
-    console.log(`Congratulations, ${userName}!`);
+    console.log(`Congratulations, ${username}!`);
+  }
+};
+
+/*
+/ BRAIN-CALC
+*/
+
+const ruleForBrainCalc = 'What is the result of expression?\nPlease input only numbers otherwise your answer will be considered as "NaN" (not a number).';
+
+const collOfOperators = ['+', '-', '*'];
+
+const getRandomItemOfArray = () => getRandomNumber(0, 3);
+// random index of coll
+
+const getRandomOperator = () => {
+  return collOfOperators[getRandomItemOfArray()];
+};
+
+const getRandomOfExpression = () => { // example return: 4 + 2
+  const stringOfExpression = `${getRandomNumber(1, 10)} ${getRandomOperator()} ${getRandomNumber(1, 10)}`;
+  return stringOfExpression
+};
+
+const collOfRandomExpressions = [getRandomOfExpression(), getRandomOfExpression(), getRandomOfExpression()];
+// 3 items of array => 3 questions to user
+
+const isCorrect = (answer, expression) => {
+  if (answer === expression) {
+    return true;
+  }
+  return false;
+};
+
+export const brainCalc = () => {
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+  console.log(ruleForBrainCalc);
+
+  let result;
+  // need to use "for...of" in this function instead of "Array.prototype.forEach()"
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of collOfRandomExpressions) {
+    // eslint-disable-next-line no-eval
+    const expression = eval(item); // initial expression returns from the coll as a 'string'
+    const answer = parseInt((readlineSync.question(`Question: ${item}\nYour answer: `)), 10);
+
+    let trueAnswer;
+
+    if (isCorrect(answer, expression)) { 
+      trueAnswer = answer;
+    } else {
+      trueAnswer = expression;
+    }
+
+    if (answer === trueAnswer) {
+      console.log('Correct!');
+      result = true;
+    } else if (answer !== trueAnswer) {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".\nLet's try again, ${username}!`);
+      result = false;
+      break;
+    }
+  }
+  if (result === true) {
+    console.log(`Congratulations, ${username}!`);
   }
 };
