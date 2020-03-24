e7787ad926955640fcc2634ca22a4ce040a8e7d9
Evgeny <evtsyga@gmail.com>
2020-03-24 20:43:20 +0300

    relocate code of brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { brainEven } from '../src/index.js';
+import brainEven from '../src/games/brain-even-source.js';
 
 console.log('Welcome to the Brain Games!\n');
 brainEven();
--- src/index.js
+++ src/index.js
@@ -5,63 +5,11 @@ const greeting = () => console.log(`Hello, ${nameRequst('May I have your name? '
 
 export default greeting;
 
-const getRandomNumber = (min, max) => {
+export const getRandomNumber = (min, max) => {
   const minNumber = min;
   const maxNumber = max;
   const random = () => Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
   return random(maxNumber);
-};
-
-/*
-/ BRAIN-EVEN
-*/
-
-const ruleForBrainEven = 'Answer "yes" if the number is even, otherwise answer "no"';
-const collOfRandomNumbers = [
-  getRandomNumber(1, 100),
-  getRandomNumber(1, 100),
-  getRandomNumber(1, 100),
-];
-// 3 items of array => 3 questions to user
-
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-};
-
-export const brainEven = () => {
-  const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}!`);
-  console.log(ruleForBrainEven);
-
-  let result;
-  // due to "break" need to use "for...of" in this function instead of "Array.prototype.forEach()"
-  // eslint-disable-next-line no-restricted-syntax
-  for (const item of collOfRandomNumbers) {
-    const answer = readlineSync.question(`Question: ${item}\nYour answer: `);
-
-    let trueAnswer;
-
-    if (isEven(item)) {
-      trueAnswer = 'yes';
-    } else if (!isEven(item)) {
-      trueAnswer = 'no';
-    }
-
-    if (answer === trueAnswer) {
-      console.log('Correct!');
-      result = true;
-    } else if (answer !== trueAnswer) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".\nLet's try again, ${username}!`);
-      result = false;
-      break;
-    }
-  }
-  if (result === true) {
-    console.log(`Congratulations, ${username}!`);
-  }
 };
 
 /*
