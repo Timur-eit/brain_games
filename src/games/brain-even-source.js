24e21e5a6442f9297ac2461f289c5b161586b7a3
Evgeny <evtsyga@gmail.com>
2020-04-02 23:55:32 +0300

    update brain-even source

--- src/games/brain-even-source.js
+++ src/games/brain-even-source.js
@@ -2,9 +2,13 @@ import { getRandomNumber, brainGames } from '../index.js';
 
 const ruleForBrainEven = 'Answer "yes" if the number is even, otherwise answer "no"';
 
-// опиши зачем нужен второй аргумент
 const isEven = (number, arg2 = null) => {
+  /** second argument was set because
+   * checking function of game engien
+   *  takes two arguments
+  */
   let result;
+
   if (arg2 === null) {
     if (number % 2 === 0) {
       result = true;
@@ -19,27 +23,27 @@ const isEven = (number, arg2 = null) => {
   return result;
 };
 
-const pointOfQuestion = getRandomNumber(1, 100);
+const collOfPointsOfQuestion = [
+  getRandomNumber(1, 100),
+  getRandomNumber(1, 100),
+  getRandomNumber(1, 100),
+];
 
-const correctAnswerForBrainEven = (num, arg2 = null) => {
+const correctAnswerForBrainEven = (answer, num) => {
   let trueAnswer;
-
-  if (arg2 === null) {
-    if (isEven(num)) {
-      trueAnswer = 'yes';
-    } else {
-      trueAnswer = 'no';
-    }
-  } else if (arg2 !== null) {
-    if (isEven(num)) {
-      trueAnswer = 'yes';
-    } else {
-      trueAnswer = 'no';
-    }
+  if (answer === 'yes' && isEven(num)) {
+    trueAnswer = 'yes';
+  } else if (answer === 'no' && !isEven(num)) {
+    trueAnswer = 'no';
+  } else if (answer !== 'yes' && isEven(num)) {
+    trueAnswer = 'yes';
+  } else if (answer !== 'no' && !isEven(num)) {
+    trueAnswer = 'no';
   }
   return trueAnswer;
 };
 
-const brainEven = () => brainGames(ruleForBrainEven, pointOfQuestion, correctAnswerForBrainEven, 'string');
+
+export const brainEven = () => brainGames(ruleForBrainEven, collOfPointsOfQuestion, correctAnswerForBrainEven, 'string');
 
 export default brainEven;
