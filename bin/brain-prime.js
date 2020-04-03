d24e319f17094c1f581a4c7d8046cdb6ebbf19ac
Evgeny <evtsyga@gmail.com>
2020-04-03 23:39:18 +0300

    rewritten brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import brainPrime from '../src/games/brain-prime-source.js';
+import { brainPrime } from '../src/games/brain-prime-source.js';
 
-console.log('Welcome to the Brain Games!');
 brainPrime();
--- src/games/brain-prime-source.js
+++ src/games/brain-prime-source.js
@@ -1,51 +1,46 @@
-// import readlineSync from 'readline-sync';
-// import { getRandomNumber } from '../index.js';
+import { getRandomNumber, brainGames } from '../index.js';
 
-// const ruleForBrainPrime = 'Answer "yes" if given number is prime, otherwise answer "no".';
+const ruleForBrainPrime = 'Answer "yes" if given number is prime, otherwise answer "no".';
 
-/**
-const collOfRandomNumbers = [
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const collOfPointsOfQuestion = [
   getRandomNumber(1, 100),
   getRandomNumber(1, 100),
   getRandomNumber(1, 100),
 ];
-*/
 
-/**
-const brainPrime = () => {
-  const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}!`);
-  console.log(ruleForBrainPrime);
-
-  let result;
-  // due to "break" need to use "for...of" in this function instead of "Array.prototype.forEach()"
-  // eslint-disable-next-line no-restricted-syntax
-  for (const item of collOfRandomNumbers) {
-    const answer = readlineSync.question(`Question: ${item}\nYour answer: `);
-
-    let trueAnswer;
-
-    if (isPrime(item)) {
-      trueAnswer = 'yes';
-    } else if (!isPrime(item)) {
-      trueAnswer = 'no';
-    }
-
-    if (answer === trueAnswer) {
-      console.log('Correct!');
-      result = true;
-    } else if (answer !== trueAnswer) {
-      console.log(
-`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".\nLet's try again, ${username}!`
-        );
-      result = false;
-      break;
-    }
+const correctAnswerForBrainPrime = (answer, num) => {
+  let trueAnswer;
+  if (answer === 'yes' && isPrime(num)) {
+    trueAnswer = 'yes';
+  } else if (answer === 'no' && !isPrime(num)) {
+    trueAnswer = 'no';
+  } else if (answer !== 'yes' && isPrime(num)) {
+    trueAnswer = 'yes';
+  } else if (answer !== 'no' && !isPrime(num)) {
+    trueAnswer = 'no';
   }
-  if (result === true) {
-    console.log(`Congratulations, ${username}!`);
-  }
+  return trueAnswer;
+};
+
+export const brainPrime = () => {
+  brainGames(
+    ruleForBrainPrime,
+    collOfPointsOfQuestion,
+    correctAnswerForBrainPrime,
+    'string',
+  );
 };
 
 export default brainPrime;
-*/
