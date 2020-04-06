0414c1583bc2ba06e4561fafcc9f3055623f5013
Evgeny <evtsyga@gmail.com>
2020-04-06 19:25:19 +0300

    delete unnecessary var from engine

--- src/index.js
+++ src/index.js
@@ -19,9 +19,7 @@ export const brainGames = (
   const username = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${username}!\n${gameRules}`);
 
-  let correctAnswerIndicator;
-
-  for (let round = 0; round < 3; round += 1) {
+  for (let round = 1; round <= 3; round += 1) {
     const itemOfPointsOfQuestion = pointOfQuestion();
 
     const answer = readlineSync.question(
@@ -40,18 +38,16 @@ export const brainGames = (
 
     if (formatedAnswer === answerForChecking) {
       console.log('Correct!');
-      correctAnswerIndicator = true;
+      if (round === 3) {
+        console.log(`Congratulations, ${username}!`);
+      }
     } else if (formatedAnswer !== answerForChecking) {
       console.log(
         `"${answer}" is wrong answer ;(.`,
         `Correct answer was "${answerForChecking}".`,
         `\nLet's try again, ${username}!`,
       );
-      correctAnswerIndicator = false;
       break;
     }
-  }
-  if (correctAnswerIndicator === true) {
-    console.log(`Congratulations, ${username}!`);
   }
 };
