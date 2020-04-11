34a3110eec2f5cdcaf0a3672e27c97fd6e523be8
Evgeny <evtsyga@gmail.com>
2020-04-11 16:33:43 +0300

    update brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,7 +2,6 @@ import { brainGames } from '../index.js';
 
 const ruleForBrainCalc = 'What is the result of expression?';
 
-
 const getRandomNumber = (min, max) => {
   const minNumber = min;
   const maxNumber = max;
@@ -12,17 +11,11 @@ const getRandomNumber = (min, max) => {
 
 const collOfOperators = ['+', '-', '*'];
 
-const getRandomOfExpression = () => { // for example: '4 + 2'
-  const stringOfExpression = `${getRandomNumber(1, 10)} ${collOfOperators[getRandomNumber(0, 3)]} ${getRandomNumber(1, 10)}`;
-  return stringOfExpression;
-};
-
 const toCompute = (string) => {
   const coll = string.split(' ');
   const a = parseInt(coll[0], 10);
   const b = parseInt(coll[2], 10);
   let result;
-
   if (coll.includes('+')) {
     result = a + b;
   } else if (coll.includes('-')) {
@@ -33,23 +26,12 @@ const toCompute = (string) => {
   return result;
 };
 
-const getCorrectAnswer = (expression) => toCompute(expression);
-
-const checkAnswer = (answer, question) => {
-  if (String(answer) === String(getCorrectAnswer(question))) {
-    return true;
-  }
-  return false;
+const getRandomOfExpression = () => { // for example: '4 + 2'
+  const stringOfExpression = `${getRandomNumber(1, 10)} ${collOfOperators[getRandomNumber(0, 3)]} ${getRandomNumber(1, 10)}`;
+  const answer = toCompute(stringOfExpression).toString();
+  return [stringOfExpression, answer];
 };
 
-
-const brainCalc = () => {
-  brainGames(
-    ruleForBrainCalc,
-    getRandomOfExpression,
-    getCorrectAnswer,
-    checkAnswer,
-  );
-};
+const brainCalc = () => brainGames(ruleForBrainCalc, getRandomOfExpression);
 
 export default brainCalc;
