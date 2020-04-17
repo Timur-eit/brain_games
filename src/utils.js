b4113f2bf6c3616e5a274b01892ba8737c2d7c68
Evgeny <evtsyga@gmail.com>
2020-04-17 12:21:57 +0300

    add src/util.js

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+const getRandomNumber = (min, max) => {
+  const minNumber = min;
+  const maxNumber = max;
+  const random = () => Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+  return random(maxNumber);
+};
+
+export default getRandomNumber;
