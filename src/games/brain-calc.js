720d4bcf5fd90415d07f2b1c636026b122cd3880
Evgeny <evtsyga@gmail.com>
2020-04-17 12:29:44 +0300

    update brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,29 +1,18 @@
 import { brainGames } from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const ruleForBrainCalc = 'What is the result of expression?';
 
-const getRandomNumber = (min, max) => {
-  const minNumber = min;
-  const maxNumber = max;
-  const random = () => Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
-  return random(maxNumber);
-};
-
-const toCompute = (firstNumber, secondNumber, operator) => {
-  let result;
+const calculate = (firstNumber, secondNumber, operator) => {
   switch (operator) {
     case '+':
-      result = firstNumber + secondNumber;
-      break;
+      return firstNumber + secondNumber;
     case '-':
-      result = firstNumber - secondNumber;
-      break;
+      return firstNumber - secondNumber;
     case '*':
-      result = firstNumber * secondNumber;
-      break;
-    default:
+      return firstNumber * secondNumber;
+    default: return null;
   }
-  return result;
 };
 
 const getRandomOfExpression = () => {
@@ -32,7 +21,7 @@ const getRandomOfExpression = () => {
   const secondNumber = getRandomNumber(1, 10);
   const operator = collOfOperators[getRandomNumber(0, collOfOperators.length)];
   const stringOfExpression = `${firstNumber} ${operator} ${secondNumber}`;
-  const answer = toCompute(firstNumber, secondNumber, operator).toString();
+  const answer = calculate(firstNumber, secondNumber, operator).toString();
   return [stringOfExpression, answer];
 };
 
