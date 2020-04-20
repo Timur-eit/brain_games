6d32d38015568a46f8dbaa61a933d708c5feb048
Evgeny <evtsyga@gmail.com>
2020-04-20 21:57:37 +0300

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,3 @@
-import readlineSync from 'readline-sync';
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-export const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}!`);
-};
+export default getRandomNumber;
