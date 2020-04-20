41fd55b562847824a2398f6ee7875bb233c70421
Evgeny <evtsyga@gmail.com>
2020-04-20 21:55:06 +0300

    update brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,16 +1,16 @@
 import brainGames from '../index.js';
-import { getRandomNumber } from '../utils.js';
+import getRandomNumber from '../utils.js';
 
-const ruleForBrainEven = 'Answer "yes" if the number is even, otherwise answer "no"';
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 
 const isEven = (number) => number % 2 === 0;
 
-const getEvenNumberAndAnswer = () => {
-  const numberForUser = getRandomNumber(1, 100);
-  const answer = isEven(numberForUser) ? 'yes' : 'no';
-  return [numberForUser, answer];
+const getRound = () => {
+  const question = getRandomNumber(1, 100);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
-const runBrainEven = () => brainGames(ruleForBrainEven, getEvenNumberAndAnswer);
+const runBrainEven = () => brainGames(description, getRound);
 
 export default runBrainEven;
