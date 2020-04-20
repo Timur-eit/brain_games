c6bc6be4a065115ed68d8c834e3a9337903622b9
Evgeny <evtsyga@gmail.com>
2020-04-20 15:16:57 +0300

    update brain-brogression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,43 +1,39 @@
-import { brainGames } from '../index.js';
-import getRandomNumber from '../utils.js';
+import brainGames from '../index.js';
+import { getRandomNumber } from '../utils.js';
 
 const ruleForBrainProgress = 'What number is missing in the progression?.';
 
-const getArithmProgressionAndAnswer = (startNumber, step) => {
-  const result = [startNumber];
+const getProgressionAndAnswer = (startNumber, step) => {
+  const progression = [startNumber];
   for (let i = 1; i < 10; i += 1) {
-    result.push(startNumber + step * i);
+    progression.push(startNumber + step * i);
   }
-  return result;
-};
 
-const getProgressionWithHiddenNumber = (coll, index) => {
-  const result = [];
-  for (let i = 0; i < coll.length; i += 1) {
-    let item = coll[i];
-    if (i === index) {
-      item = '..';
+  const hiddenNumber = progression[getRandomNumber(0, progression.length)];
+  const progressionWithHiddenNumber = progression.map((item) => {
+    let numberOfProgression = item;
+    if (numberOfProgression === hiddenNumber) {
+      numberOfProgression = '..';
     }
-    result.push(item);
-  }
-  const progression = result;
-  const answer = coll[index];
-  return [progression, answer];
+    return numberOfProgression;
+  });
+
+  return [progressionWithHiddenNumber, hiddenNumber];
 };
 
-const getQuestionAndAnswer = () => {
-  const progression = getArithmProgressionAndAnswer(getRandomNumber(1, 15), getRandomNumber(1, 5));
-  const [question, answer] = getProgressionWithHiddenNumber(progression, getRandomNumber(1, 9));
 
-  const questionString = question.join(' ');
-  const answerString = answer.toString();
+const getProgressionAndAnswerForGame = () => {
+  const [question, answer] = getProgressionAndAnswer(getRandomNumber(1, 15), getRandomNumber(1, 5));
+  const progressionForUser = question.join(' ');
+  const answerForGameEngine = answer.toString();
 
-  return [questionString, answerString];
+  return [progressionForUser, answerForGameEngine];
 };
 
-const brainProgresion = () => brainGames(
+
+const runBrainProgresion = () => brainGames(
   ruleForBrainProgress,
-  getQuestionAndAnswer,
+  getProgressionAndAnswerForGame,
 );
 
-export default brainProgresion;
+export default runBrainProgresion;
