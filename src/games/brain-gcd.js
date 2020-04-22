d2a1f91ab2683b6523e65f614bb8996a2cb7dc4b
Evgeny <evtsyga@gmail.com>
2020-04-22 13:03:12 +0300

    update brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,27 +1,23 @@
-import brainGames from '../index.js';
+import gameEngine from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGCD = (x, y) => {
-  // Euclidean algorithm
-  if (y > x) {
-    return getGCD(y, x);
-  }
+const getGcd = (x, y) => {
   if (!y) {
     return x;
   }
-  return getGCD(y, x % y);
+  return getGcd(y, x % y);
 };
 
 const getRound = () => {
   const firstNumber = getRandomNumber(1, 20);
   const secodNumber = getRandomNumber(1, 20);
   const question = `${firstNumber} ${secodNumber}`;
-  const answer = getGCD(firstNumber, secodNumber).toString();
+  const answer = getGcd(firstNumber, secodNumber).toString();
   return [question, answer];
 };
 
-const runGcd = () => brainGames(description, getRound);
+const runGcd = () => gameEngine(description, getRound);
 
 export default runGcd;
