f2a6c58771c1db9d87ab8755cd7a428d68896a7d
Evgeny <evtsyga@gmail.com>
2020-04-23 12:54:14 +0300

    update brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,9 +5,10 @@ const description = 'What number is missing in the progression?.';
 
 const getQuestion = (startNumber, step, hiddenIndex) => {
   const progression = [];
-  for (let i = 0; i <= 9; i += 1) {
-    let item = startNumber + step * i;
-    if (i === hiddenIndex) {
+  const maxProgressionIndex = 9;
+  for (let progressionIndex = 0; progressionIndex <= maxProgressionIndex; progressionIndex += 1) {
+    let item = startNumber + step * progressionIndex;
+    if (progressionIndex === hiddenIndex) {
       item = '..';
     }
     progression.push(item);
@@ -17,9 +18,10 @@ const getQuestion = (startNumber, step, hiddenIndex) => {
 
 
 const getRound = () => {
+  const progressionLength = 10;
   const startNumber = getRandomNumber(1, 15);
   const step = getRandomNumber(1, 5);
-  const hiddenIndex = getRandomNumber(0, 10);
+  const hiddenIndex = getRandomNumber(0, progressionLength);
 
   const question = getQuestion(
     startNumber,
